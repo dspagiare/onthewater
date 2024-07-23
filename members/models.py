03b4f61133ef2f279b1594a3072761a3f967cfc4
Dan Spagiare <dspagiare@gmail.com>
2024-07-23 16:22:12 -0400

    connected to db

--- members/models.py
+++ members/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    city_name = models.CharField(max_length=200)
+    zipcode   = models.CharField(max_length=200)
+    state     = models.CharField(max_length=200)
+    member_since = models.DateTimeField("date joined")
--- members/views.py
+++ members/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
 # Create your views here.
+def testPage(request) :
+    return HttpResponse("Test page for Django On The Water App")
